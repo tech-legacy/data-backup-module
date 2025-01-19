c701aa45054f18b825b80119323dba51dfb78510
zxffffffff <34299738+zxffffffff@users.noreply.github.com>
2025-01-19 13:43:26 +0800

    引入 2nd-party

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 ![workflow](https://github.com/tech-legacy/data-backup-module/actions/workflows/build-ubuntu.yml/badge.svg?event=push)
 
 分布式数据加密备份
+
+## Features
+
+- [x] Automatic File Backup 自动文件备份
+- [ ] Source Folder 待备份文件夹
+- [ ] Destination Folder 备份目标文件夹 (可选压缩、加密)
+- [ ] Temporary Decryption Folder 临时解密文件夹 (备份数据解密时可选仅保留24小时)
--- test/common_test.cpp
+++ test/common_test.cpp
@@ -6,11 +6,7 @@
 **
 ****************************************************************************/
 #include <gtest/gtest.h>
-#include "common.h"
-#include "cpp_def.h"
-#include <thread>
-#include <sstream>
-#include <unordered_set>
+#include "Cryptography.h"
 
 #if defined(_MSC_VER) && (_MSC_VER >= 1500 && _MSC_VER < 1900)
 /* msvc兼容utf-8: https://support.microsoft.com/en-us/kb/980263 */
@@ -20,102 +16,21 @@
 #pragma warning(disable : 4566)
 #endif
 
-TEST(Common, Time)
+TEST(Cryptography, Hash_SHA)
 {
-    auto start = Common::NowSinceEpoch_MS();
-    ASSERT_GT(start, 0);
+    std::string str = "XDFGHJafhdldknf@p9US*jknbgKSQ!~!@#$%^&*()_+}\"?><MNBVCXJHGV>NHBV-";
 
-    Common::Sleep(100);
-    auto now = Common::NowSinceEpoch_MS();
-    ASSERT_GE(now, start + 100);
-
-    std::string s = Common::TimeFormat(start);
-    ASSERT_NE(s.find("-"), std::string::npos);
-    ASSERT_NE(s.find(" "), std::string::npos);
-    ASSERT_NE(s.find(":"), std::string::npos);
-    ASSERT_NE(s.find("."), std::string::npos);
-
-    std::string s2 = Common::TimeFormat(now);
-    ASSERT_LT(s, s2);
-}
-
-// TEST(Common, GenUuid)
-// {
-//     std::string uuid = Common::GenUuid();
-//     std::string uuid2 = Common::GenUuid();
-//     std::cout << VAR_NAME(uuid) << ": " << uuid << std::endl;
-//     std::cout << VAR_NAME(uuid2) << ": " << uuid2 << std::endl;
-//     ASSERT_FALSE(uuid.empty());
-//     ASSERT_NE(uuid, uuid2);
-
-//     std::unordered_set<std::string> uuids;
-//     const int cnt = 1000 * 1000;
-//     for (int i = 0; i < cnt; ++i)
-//     {
-//         std::string uuid3 = Common::GenUuid();
-//         if (i == 0)
-//             std::cout << VAR_NAME(uuid3) << ": " << uuid3 << std::endl;
-//         uuids.insert(uuid3);
-//     }
-//     ASSERT_EQ(uuids.size(), cnt);
-// }
-
-TEST(Common, GenUuid2)
-{
-    std::string uuid = Common::GenUuid2();
-    std::string uuid2 = Common::GenUuid2();
-    std::cout << VAR_NAME(uuid) << ": " << uuid << std::endl;
-    std::cout << VAR_NAME(uuid2) << ": " << uuid2 << std::endl;
-    ASSERT_FALSE(uuid.empty());
-    ASSERT_NE(uuid, uuid2);
-
-    std::unordered_set<std::string> uuids;
-    const int cnt = 1000 * 1000;
-    for (int i = 0; i < cnt; ++i)
+    for (int i = 0; i < 10; ++i)
     {
-        std::string uuid3 = Common::GenUuid2();
-        if (i == 0)
-            std::cout << VAR_NAME(uuid3) << ": " << uuid3 << std::endl;
-        uuids.insert(uuid3);
-    }
-    EXPECT_GT(uuids.size(), cnt * 0.999);
-}
+        constexpr size_t bits = 256;
 
-TEST(Common, Random)
-{
-    std::unordered_set<int> nums;
-    const int cnt = 1000 * 1000;
-    for (int i = 0; i < cnt; ++i)
-    {
-        nums.insert(Common::Random());
-    }
-    /* 会有重复，尽可能的随机就行 */
-    EXPECT_GT(nums.size(), cnt * 0.999);
-}
+        StringBuffer buf(str.data(), str.size());
 
-TEST(Common, toThreadLocalStr)
-{
-    auto test = [](int i)
-    {
-        for (int j = 0; j < 10000; ++j)
-        {
-            std::stringstream ss;
-            ss << std::this_thread::get_id() << "-" << i << "-" << j;
-            std::string s = ss.str();
-            const char *p = Common::toThreadLocalStr(s);
-            ASSERT_EQ(s, std::string(p));
-            // std::cerr << s << std::endl;
-        }
-    };
+        StringBuffer hash = Cryptography::Hash::SHA(buf, bits);
+        StringBuffer hash2 = Cryptography::Hash::SHA(buf, bits);
 
-    std::vector<std::thread> v;
-    for (int i = 0; i < 100; ++i)
-    {
-        v.emplace_back(test, i);
-    }
-    for (auto &th : v)
-    {
-        if (th.joinable())
-            th.join();
+        EXPECT_EQ(hash, hash2);
+        EXPECT_NE(buf, hash);
+        str += str;
     }
 }
