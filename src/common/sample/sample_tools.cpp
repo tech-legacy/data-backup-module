68101fd9ff93ad959cef0c20ced496e7cad4f2ad
zxffffffff <34299738+zxffffffff@users.noreply.github.com>
2025-01-19 14:09:36 +0800

    屏蔽 2nd-party

--- src/common/sample/sample_tools.cpp
+++ src/common/sample/sample_tools.cpp
@@ -5,7 +5,7 @@
 ** Support	: zxffffffff@outlook.com, 1337328542@qq.com
 **
 ****************************************************************************/
-#include "platform.h"
+#include "../platform.h"
 
 #if defined(_MSC_VER) && (_MSC_VER >= 1500 && _MSC_VER < 1900)
 /* msvc兼容utf-8: https://support.microsoft.com/en-us/kb/980263 */
--- test/common_test.cpp
+++ test/common_test.cpp
@@ -6,7 +6,6 @@
 **
 ****************************************************************************/
 #include <gtest/gtest.h>
-#include "Cryptography.h"
 
 #if defined(_MSC_VER) && (_MSC_VER >= 1500 && _MSC_VER < 1900)
 /* msvc兼容utf-8: https://support.microsoft.com/en-us/kb/980263 */
@@ -16,21 +15,7 @@
 #pragma warning(disable : 4566)
 #endif
 
-TEST(Cryptography, Hash_SHA)
+TEST(Common, test)
 {
-    std::string str = "XDFGHJafhdldknf@p9US*jknbgKSQ!~!@#$%^&*()_+}\"?><MNBVCXJHGV>NHBV-";
-
-    for (int i = 0; i < 10; ++i)
-    {
-        constexpr size_t bits = 256;
-
-        StringBuffer buf(str.data(), str.size());
-
-        StringBuffer hash = Cryptography::Hash::SHA(buf, bits);
-        StringBuffer hash2 = Cryptography::Hash::SHA(buf, bits);
-
-        EXPECT_EQ(hash, hash2);
-        EXPECT_NE(buf, hash);
-        str += str;
-    }
+    ASSERT_EQ(1 + 1, 2);
 }
